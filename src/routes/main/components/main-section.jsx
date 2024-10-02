d4f30574fef09650b7722a1435f7417b4db97af0
almighty5 <almighty5@snu.ac.kr>
2024-10-02 11:55:29 +0900

    :sparkles: 양도글 작성 화면 구현

--- src/routes/main/components/main-section.jsx
+++ src/routes/main/components/main-section.jsx
@@ -1,7 +1,7 @@
 export const MainSection = () => {
   return (
     <div className="flex flex-col px-10 items-center justify-center">
-      <h1>서비스 설명입니다.</h1>
+      <h2>서비스 설명입니다.</h2>
     </div>
   );
 };
