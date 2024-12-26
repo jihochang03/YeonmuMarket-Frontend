98749576ee0457dfc32c0cdc2425d329ee817c78
오수빈 Subin OH <155422694+almighty55555@users.noreply.github.com>
2024-12-26 18:55:43 +0900

    Revert "디자인 수정"

--- src/routes/main/components/sold-tickets/promo-form.jsx
+++ src/routes/main/components/sold-tickets/promo-form.jsx
@@ -173,7 +173,7 @@ function PromoForm({ ticket, onSave, onCancel }) {
           }
         />
       </div>
-      <div className="flex w-full justify-around items-center gap-2 pb-6">
+      <div className="flex w-full justify-around items-center gap-2 pb-24">
         {/* <button
           type="button"
           className="flex items-center gap-1 bg-black text-white px-4 py-2 rounded-md"
