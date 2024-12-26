d7c1eb79d8467efe250fc1282a142323b4c0c6b0
hoyah <hoyaho03@snu.ac.kr>
2024-12-27 08:57:58 +0900

    리디렉트 오류 수정

--- src/routes/login/components/signup/signup-form.jsx
+++ src/routes/login/components/signup/signup-form.jsx
@@ -1,9 +1,7 @@
-// src/routes/login/components/signup/signup-form.jsx
-
 import React from "react";
 import { KakaoButton } from "../kakao-button";
 
-export const SignUpForm = (redirectUrl) => {
+export const SignUpForm = ({ redirectUrl }) => {
   return (
     <div className="w-full flex flex-col justify-start items-center gap-5">
       <KakaoButton isLogin={false} redirectUrl={redirectUrl} />
